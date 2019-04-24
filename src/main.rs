07bf38a247b98031e2a4ece5217e2b321437ef59
JNhua <hjntc59@qq.com>
2019-04-24 21:10:04 +0800

    Blockchain native

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ extern crate time;
 extern crate crypto;
 extern crate hex;
 extern crate json;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate canteen;
+extern crate urlparse;
 
+use urlparse::urlparse;
+use canteen::*;
+use canteen::utils;
 use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
 use ::hex::{FromHex, ToHex};
@@ -10,6 +19,7 @@ use ::hex::{FromHex, ToHex};
 use ::time::*;
 use std::rc::Rc;
 use rustc_serialize::hex::FromHex;
+use std::thread::current;
 
 
 struct Transaction {
@@ -33,7 +43,7 @@ struct Blockchain {
 }
 
 impl Block {
-    fn encode(&self) -> String {
+    fn dumps(&self) -> String {
         let mut str = String::new();
         str.push_str(&String::from(self.index));
         str.push_str(&String::from(self.timestamp));
@@ -45,11 +55,11 @@ impl Block {
         str
     }
 
-    fn myhash(&self) -> String {
+    fn myhash(block: Block) -> String {
         // create a SHA3-256 object
         let mut hasher = Sha3::sha3_256();
         // write input message
-        hasher.input_str(&self.encode());
+        hasher.input_str(&block.dumps());
         // read hash hexdigest
         hasher.result_str()
     }
@@ -66,6 +76,29 @@ impl Blockchain {
 
     fn init(&mut self) {
         self.new_block(100, Some(String::from("1")));
+    }
+
+    fn register_node(&mut self, address: String) {
+        /*
+        增加新节点
+        ：参数address:node的address. Eg. 'http://127.0.0.1:5000'
+        */
+        parsed_url = urlparse(address);
+        self.nodes.push_str(parsed_url.netloc);
+    }
+
+    fn valid_chain(chain: Vec<Block>) -> bool {
+        let last_block = &chain[0];
+        let mut current_idx = 1;
+
+        while current_idx < chain.len() {
+            let block = &chain[current_idx];
+            println!("{:?}", last_block);
+            println!("{:?}", block);
+            println!("\n----------------\n");
+            if block.previous_hash != block.myhash(last_block) {}
+        };
+        return true
     }
 
     fn new_block(&mut self, proof: i16,
