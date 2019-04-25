00acce5f8b2291b94cd33686ea71ad60d532434b
JNhua <hjntc59@qq.com>
2019-04-25 14:09:31 +0800

    test canteen

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
 extern crate time;
 extern crate crypto;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-extern crate canteen;
 extern crate urlparse;
 extern crate requests;
 extern crate rustc_serialize;
 extern crate uuid;
+extern crate canteen;
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+use canteen::{Canteen, Request, Response, Method};
+use canteen::utils;
 
 use urlparse::urlparse;
 use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
-use ::time::*;
+use ::time::get_time;
 use std::rc::Rc;
 use requests::*;
 use rustc_serialize::json;
-use uuid::*;
+use uuid::Uuid;
 
-
+#[derive(RustcDecodable, RustcEncodable, Debug, Clone)]
 struct Transaction {
     sender: String,
     recipient: String,
     amount: i8,
 }
 
+#[derive(RustcDecodable, RustcEncodable, Debug, Clone)]
 struct Block {
     index: i8,
     timestamp: i64,
-    transaction: Transaction,
+    transaction: Vec<Transaction>,
     proof: i32,
     previous_hash: String,
 }
@@ -44,18 +49,17 @@ impl Block {
     /*
     fn dumps(&self) -> String {
         let mut str = String::new();
-        str.push_str(&String::from(self.index));
-        str.push_str(&String::from(self.timestamp));
-        str.push_str(&self.transaction.sender);
-        str.push_str(&self.transaction.recipient);
-        str.push_str(&self.transaction.amount);
-        str.push_str(&String::from(self.proof));
+        str.push_str(&(json::encode(&self.index).unwrap()
+        +&json::encode(&self.timestamp).unwrap()
+            +&json::encode(&self.transaction).unwrap())
+        );
+        str.push_str(&json::encode(&self.proof).unwrap());
         str.push_str(&self.previous_hash);
         str
     }
     */
 
-    fn myhash(block: Block) -> String {
+    pub fn myhash(block: &Block) -> String {
         // create a SHA3-256 object
         let mut hasher = Sha3::sha3_256();
         // write input message
@@ -65,6 +69,7 @@ impl Block {
     }
 }
 
+//关联函数
 impl Blockchain {
     fn new() -> Blockchain {
         Blockchain {
@@ -74,33 +79,30 @@ impl Blockchain {
         }
     }
 
-    fn init(&mut self) {
-        self.new_block(100, Some(String::from("1")));
+    fn valid_proof(&last_proof: &i32, &proof: &i32) -> bool {
+        // create a SHA3-256 object
+        let mut hasher = Sha3::sha3_256();
+        // write input message
+        hasher.input_str(&(json::encode(&last_proof).unwrap() + &json::encode(&proof).unwrap()));
+
+        let guess_hash = hasher.result_str();
+        return guess_hash[0..4] == String::from("0000");
     }
 
-    fn register_node(&mut self, address: String) {
-        /*
-        增加新节点
-        ：参数address:node的address. Eg. 'http://127.0.0.1:5000'
-        */
-        parsed_url = urlparse(address);
-        self.nodes.push(parsed_url);
-    }
-
-    fn valid_chain(chain: Vec<Block>) -> bool {
+    fn valid_chain(chain: &Vec<Block>) -> bool {
         let mut last_block = &chain[0];
         let mut current_idx = 1;
 
         while current_idx < chain.len() {
             let block = &chain[current_idx];
-            println!("{:?}", last_block);
-            println!("{:?}", block);
+            println!("{:#?}", last_block);
+            println!("{:#?}", block);
             println!("\n----------------\n");
-            if block.previous_hash != block.myhash(last_block) {
+            if block.previous_hash != Block::myhash(last_block) {
                 return false;
             }
 
-            if !valid_proof(last_block.proof, block.proof) {
+            if !Blockchain::valid_proof(&last_block.proof, &block.proof) {
                 return false;
             }
 
@@ -110,6 +112,35 @@ impl Blockchain {
         true
     }
 
+    fn timestamp() -> i64 {
+        let timespec = get_time();
+        timespec.sec * 1000 + (timespec.nsec as f64 / 1000.0 / 1000.0) as i64
+    }
+
+    fn proof_of_work(last_proof: i32) -> i32 {
+        let mut proof = 0;
+        while Blockchain::valid_proof(&last_proof, &proof) == false {
+            proof += 1;
+        }
+        proof
+    }
+}
+
+//方法
+impl Blockchain {
+    fn init(&mut self) {
+        self.new_block(100, Some(String::from("1")));
+    }
+
+    fn register_node(&mut self, address: String) {
+        /*
+        增加新节点
+        ：参数address:node的address. Eg. 'http://127.0.0.1:5000'
+        */
+        let parsed_url = urlparse(address);
+        self.nodes.push(parsed_url.netloc);
+    }
+
     fn resolve_conflicts(&mut self) -> bool {
         let neighbours = &self.nodes;
         let mut new_chain = vec![];
@@ -117,14 +148,14 @@ impl Blockchain {
 
         for node in neighbours {
             let response =
-                requests::get("http://" + String::from(node) + "/chain").unwrap();
+                requests::get("http://".to_string() + node + "/chain").unwrap();
 
-            if response.status_code() == 200 {
+            if response.status_code() == requests::StatusCode::Ok {
                 let res_json = response.json().unwrap();
                 let length = res_json["length"].as_usize().unwrap();
-                let chain = res_json["chain"];
+                let chain = json::decode(res_json["chain"].as_str().unwrap()).unwrap();
 
-                if length > max_length && valid_chain(chain) {
+                if length > max_length && Blockchain::valid_chain(&chain) {
                     max_length = length;
                     new_chain = chain;
                 }
@@ -142,18 +173,22 @@ impl Blockchain {
         self.current_transactions.clear();
         let new_block = Block {
             index: (self.chain.len() + 1) as i8,
-            timestamp: timestamp(),
-            transaction: self.transaction,
+            timestamp: Blockchain::timestamp(),
+            transaction: self.current_transactions.clone(),
             proof,
             previous_hash: match previous_hash {
                 None => {
-                    myhash(self.chain.last())
+                    Block::myhash(self.chain.last().unwrap())
                 }
                 Some(val) => val,
             },
         };
-        self.chain.push(Rc::clone(&new_block));
+        self.chain.push(new_block.clone());
         new_block
+    }
+
+    fn last_block(&self) -> &Block {
+        return self.chain.last().unwrap();
     }
 
     fn new_transaction(&mut self, sender: String, recipient: String, amount: i8) -> i8 {
@@ -164,64 +199,13 @@ impl Blockchain {
         });
         self.last_block().index + 1
     }
-
-    fn last_block(&self) -> &Block {
-        return self.chain.last().unwrap();
-    }
-
-    fn timestamp() -> i64 {
-        let timespec = time::get_time();
-        timespec.sec * 1000 + (timespec.nsec as f64 / 1000.0 / 1000.0) as i64
-    }
-
-    fn proof_of_work(last_proof: i32) -> i32 {
-        let proof = 0;
-        while valid_proof(last_proof, proof) == false {
-            proof += 1;
-        }
-        proof
-    }
-
-    fn valid_proof(last_proof: i32, proof: i32) -> bool {
-        // create a SHA3-256 object
-        let mut hasher = Sha3::sha3_256();
-        // write input message
-        hasher.input_str(&(String::from(last_proof) + String::from(proof)));
-
-        let guess_hash = hasher.result_str();
-        return guess_hash[0..4] == "0000";
-    }
 }
 
-use canteen::*;
-use canteen::utils;
-use canteen::{Response, Request};
 
-fn mine(req: &Request) -> Response {
-    let mut res = Response::new();
-
-    res.set_status(200);
-    res.set_content_type("text/plain");
-    res.append("Hello, world!");
-
-    res
-}
 
 
 fn main() {
     let mut blockchian = Blockchain::new();
     blockchian.init();
-    let mut cnt = Canteen::new();
-    let node_identifier = String::from(uuid::Uuid::new_v4()).replace("-", "");
-
-    // bind to the listening address
-    cnt.bind(("127.0.0.1", 5000));
-
-    // set the default route handler to show a 404 message
-    cnt.set_default(utils::err_404);
-
-    // respond to requests to / with "Hello, world!"
-    cnt.add_route("/mine", &[Method::Get], mine);
-
-    cnt.run();
+    let node_identifier = Uuid::new_v4().to_string().replace("-", "");
 }
